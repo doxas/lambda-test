ff013b23b8ef0f92a9dc188ab72c1897b0cb690e
doxas <eizanco@gmail.com>
2022-01-13 17:04:21 +0900

    implement new api

--- handler.js
+++ handler.js
@@ -1,3 +1,5 @@
 import { postal as postalFunction } from './src/postal.js';
+import { weather as weatherFunction } from './src/weather.js';
 
 export const postal = postalFunction;
+export const weather = weatherFunction;
